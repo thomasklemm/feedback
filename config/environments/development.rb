a53d894dbac958a391fce290e951a1cd8d7192b9
Thomas Klemm <github@tklemm.eu>
2012-07-13 16:25:27 +0200

    Basic styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,4 @@ Knight::Application.configure do
   # Quiet Assets (Mute asset pipeline log messages)
   config.quiet_assets = true
 
-  # Nicely formatted Sass Output
-  config.sass.style = :nested
-
 end
